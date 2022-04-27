f353457cdf7d2de5cf45995b6e78328053fedae5
scottbooma <scottbooma@gmail.com>
2022-04-27 17:51:15 -0400

    Fix item quality calculations

--- dist/index.3d214d75.js
+++ dist/index.3d214d75.js
@@ -615,30 +615,30 @@ function sellInDegradation(item) {
 }
 function qualityDegradation(item) {
     if (item.name.includes("Aged Brie")) {
-        item.quality = item.quality + (today - item.dateAdded);
+        item.quality = +item.quality + (today - item.dateAdded);
         return item;
     } else if (item.name.includes("Sulfuras")) {
         item.quality = 80;
         return item;
     } else if (item.name.includes("Conjured")) {
-        item.quality = item.quality - double(today - item.dateAdded);
+        item.quality = +item.quality - double(today - item.dateAdded);
         return item;
     } else if (item.name.includes("Backstage pass")) {
         if (item.sellIn > 10) {
-            item.quality = item.quality + (today - item.dateAdded);
+            item.quality = +item.quality + (today - item.dateAdded);
             return item;
         } else if (item.sellIn <= 10 && item.sellIn > 5) {
-            item.quality = item.quality + double(today - item.dateAdded);
+            item.quality = +item.quality + double(today - item.dateAdded);
             return item;
         } else if (item.sellIn <= 5 && item.sellIn > 0) {
-            item.quality = item.quality + triple(today - item.dateAdded);
+            item.quality = +item.quality + triple(today - item.dateAdded);
             return item;
         } else {
             item.quality = 0;
             return item;
         }
     } else {
-        item.quality = item.quality - (today - item.dateAdded);
+        item.quality = +item.quality - (today - item.dateAdded);
         return item;
     }
 }
